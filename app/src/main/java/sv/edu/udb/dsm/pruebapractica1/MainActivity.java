a39b38a4b3835cb0e3a61888753e9206d9e91a87
Giovanny Abrego <gm.abregoh@gmail.com>
2022-03-06 07:33:09 -0600

    agrega shared preference login y menu

--- app/src/main/java/sv/edu/udb/dsm/pruebapractica1/MainActivity.java
+++ app/src/main/java/sv/edu/udb/dsm/pruebapractica1/MainActivity.java
@@ -2,13 +2,43 @@ package sv.edu.udb.dsm.pruebapractica1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText campoUsuario,campoPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        campoUsuario = (EditText) findViewById(R.id.txtUsuario);
+        campoPass = (EditText) findViewById(R.id.txtPassword);
+
+
+    }
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.btnIngresar:
+                guardarPreferencias();
+                break;
+        }
+    }
+
+    private void guardarPreferencias() {
+        SharedPreferences preferences = getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+
+        String usuario=campoUsuario.getText().toString();
+        String pass=campoPass.getText().toString();
+
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("user",usuario);
+        editor.putString("pass",pass);
+
+        editor.commit();
     }
 }
---
+++ app/src/main/java/sv/edu/udb/dsm/pruebapractica1/Menu.java
@@ -0,0 +1,44 @@
+package sv.edu.udb.dsm.pruebapractica1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Menu extends AppCompatActivity {
+    TextView txtvUsuario;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        txtvUsuario = (TextView) findViewById(R.id.txtvUsuario);
+        cargarPreferencias();
+    }
+
+    private void cargarPreferencias() {
+        SharedPreferences preferences = getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+        String user=preferences.getString("user","No hay credenciales");
+        txtvUsuario.setText(user);
+    }
+
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.btnEjercicio1:
+                break;
+            case R.id.btnEjercicio2:
+                break;
+            case R.id.btnSalir:
+                reiniciarPreferencias();
+                finish();
+        }
+    }
+
+    private void reiniciarPreferencias() {
+        SharedPreferences preferences = getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+        preferences.edit().clear().commit();
+
+    }
+}
